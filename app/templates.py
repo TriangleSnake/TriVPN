ac645c4235010c5de06fcb8347c732cd29543b5e
TriangleSnake <trianglesnake2002@gmail.com>
2025-04-19 21:53:52 +0800

    Fix templates.py

--- app/templates.py
+++ app/templates.py
@@ -1,15 +1,15 @@
 from config import *
 
-SRV_INTERFACE = f'''
+INTERFACE = f'''
 [Interface]
-PrivateKey = {{server_private_key}}
+PrivateKey = {{private_key}}
 Address = {SRV_INT_IP}
 ListenPort = {SRV_PUB_PORT}
 '''
 
-SRV_PEER = f'''
+PEER = f'''
 [Peer]
-PublicKey = {{client_public_key}}
+PublicKey = {{public_key}}
 Endpoint = {SRV_PUB_IP}:{SRV_PUB_PORT}
 AllowedIPs = {CLIENT_ALLOW_IP}
 PersistentKeepalive = 25
